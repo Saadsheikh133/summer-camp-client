f8be1777c90c3dbd92227f0e7459148edf29f0f6
saadsheikh133 <mdsaadsheikh7096@gmail.com>
2023-06-07 00:30:56 +0600

    added navbar section

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import router from './Routes/Route.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <div className='container mx-auto'>
+    <div className='container mx-auto bg-slate-200'>
       <RouterProvider router={router} />
     </div>
   </React.StrictMode>,

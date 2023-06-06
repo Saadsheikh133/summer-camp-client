232a3215c1a9e2e94f7a015603ac94b4e7b83e00
saadsheikh133 <mdsaadsheikh7096@gmail.com>
2023-06-07 00:43:35 +0600

    added footer section and toggle login and log out btn on navbar

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -2,6 +2,12 @@ import { Link } from "react-router-dom";
 
 
 const Navbar = () => {
+    const user = 'md:saad'
+
+    const handleLogOut = () => {
+
+    }
+
     const itemInfo = <>
         <li><Link to="/">Home</Link></li>
         <li><a>Instructors</a></li>
@@ -15,19 +21,32 @@ const Navbar = () => {
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
-                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 shadow  rounded-box w-52 bg-black bg-opacity-40 text-xl">
                         {itemInfo}
                     </ul>
                 </div>
                 <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
             </div>
             <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1">
+                <ul className="menu menu-horizontal px-1 text-xl">
                     {itemInfo}
                 </ul>
             </div>
             <div className="navbar-end">
-                <a className="btn">Button</a>
+                {
+                    user ?
+                        <>
+                            <div className="tooltip tooltip-bottom" data-tip={user?.displayName}>
+                                <img className='rounded-full h-14 mr-2' src={user?.photoURL} alt="" />
+                            </div>
+                            <button onClick={handleLogOut} className='btn btn-primary'>Log Out</button>
+                        </>
+                        :
+                        <>
+
+                            <button className='btn btn-primary'><Link to="/login">Login</Link></button>
+                        </>
+                }
             </div>
         </div>
     );

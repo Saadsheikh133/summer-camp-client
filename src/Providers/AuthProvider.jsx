43b234005d7aadb1b15ed9182fde521b9521dc48
saadsheikh133 <mdsaadsheikh7096@gmail.com>
2023-06-07 08:30:15 +0600

    added authProvider page  and setup firebase authentication and implement register function

---
+++ src/Providers/AuthProvider.jsx
@@ -0,0 +1,34 @@
+import { createContext, useState } from "react";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import app from "../firebase/firebase.config";
+
+export const AuthContext = createContext(null);
+const auth = getAuth(app)
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+
+    const register = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+
+
+
+    const authInfo = {
+        user,
+        loading,
+        register,
+        
+
+    }
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;

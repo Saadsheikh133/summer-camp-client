ff4dd80018927347a8ad0220f71f71583c148c15
saadsheikh133 <mdsaadsheikh7096@gmail.com>
2023-06-10 12:13:13 +0600

    added selected class page and implement delete class

--- src/Pages/Dashboard/Student/SelectedClasses/SelectedClasses.jsx
+++ src/Pages/Dashboard/Student/SelectedClasses/SelectedClasses.jsx
@@ -1,8 +1,39 @@
 import { FaTrash } from "react-icons/fa";
 import useCard from "../../../../Hooks/useCard/useCard";
+import Swal from "sweetalert2";
 
 const SelectedClasses = () => {
-    const [card] = useCard();
+    const [card, refetch] = useCard();
+
+
+    const handleDelete = (_id) => {
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:5000/removeClasses/${_id}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        if (data.deletedCount > 0) {
+                            refetch();
+                            Swal.fire(
+                                'Deleted!',
+                                'Your file has been deleted.',
+                                'success'
+                            )
+                        }
+                    })
+            }
+        })
+    }
     return (
         <div>
             <h2 className="text-5xl text-center font-bold my-20">My Selected Classes: {card.length}</h2>
@@ -16,6 +47,7 @@ const SelectedClasses = () => {
                             <th>Name</th>
                             <th>Email</th>
                             <th>Price</th>
+                            <th>Pay</th>
                             <th>Delete</th>
                         </tr>
                     </thead>
@@ -38,7 +70,10 @@ const SelectedClasses = () => {
                                 <td>{singleClass.email}</td>
                                 <td>${singleClass.price}</td>
                                 <td>
-                                    <td className='text-white bg-red-600 btn btn-md'><FaTrash></FaTrash> </td>
+                                    <button className="btn btn-info">Pay</button>
+                                </td>
+                                <td>
+                                    <button onClick={() => handleDelete(singleClass._id)} className='text-white bg-red-600 btn btn-md'><FaTrash></FaTrash> </button>
                                 </td>
                             </tr>)
                         }
